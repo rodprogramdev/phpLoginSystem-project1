86bca5b58385a8565917b47cd98abef5d5a9d50f
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-01-21 10:45:10 +0800

    update navigation layout

--- header.php
+++ header.php
@@ -1,21 +1,24 @@
 
-<header>
-        
-  <div class="header">  
-    <div class="header__heading">
-      <h1>WELCOME!!</h1>
-    </div>   
-    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RODPROGRAMDEV LOGINPAGE</title>
+    <link rel="stylesheet"  href="css/styles.css">
   
-    <div class="nav">
-    <nav>
+</head>
+<body>
+  
+    <div class="navigation">
+      <nav>
         <ul>
             <li><a href="index.php">HOME</a></li>
             <li><a href="signup.php">SIGN UP</a></li>
             <li><a href="login.php">LOG IN</a></li>
         </ul>
-      </nav>    
+      </nav> 
     </div>
-  </div>
-       
-</header>
+    
+            
--- signup.php
+++ signup.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up Page</title>
+    <link rel="stylesheet"  href="css/styles.css">
+</head>
+<body>
+    
 <?php 
     include_once 'header.php';
 ?>
+
 <section>
     <div class="signupForm">
         <h2>SIGN UP</h2>
@@ -15,3 +27,5 @@
         </form>
     </div>
 </section>
+</body>
+</html>
