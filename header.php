917ba159806cc0b39a48ca6e2ffbbaa272fc5a7f
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-01-21 10:20:30 +0800

    update background-color and layout version

--- header.php
+++ header.php
@@ -1,17 +1,21 @@
 
 <header>
-        <div class="nav">
-            <nav>
-                <ul>
-                    <li><a href="index.php">HOME</a></li>
-                    <li><a href="signup.php">SIGN UP</a></li>
-                    <li><a href="login.php">LOG IN</a></li>
-                </ul>
-             </nav>    
-        </div>
-           
-        <div class="header">
-          <h1>WELCOME TO THIS LOGIN PAGE EXERCISE!!</h1>
-            
-        </div>
+        
+  <div class="header">  
+    <div class="header__heading">
+      <h1>WELCOME!!</h1>
+    </div>   
+    
+  
+    <div class="nav">
+    <nav>
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="signup.php">SIGN UP</a></li>
+            <li><a href="login.php">LOG IN</a></li>
+        </ul>
+      </nav>    
+    </div>
+  </div>
+       
 </header>
