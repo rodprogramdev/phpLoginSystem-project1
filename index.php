15440a86de5a03f0e3e6b4f535d6ef923e6ffdd5
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-01-21 11:37:53 +0800

    add background-image

--- index.php
+++ index.php
@@ -6,7 +6,11 @@ include_once 'header.php';
 
 <div class="header">
     <div class="header__heading">
-        <h1>WELCOME!!</h1>
+        <h1>
+            <span class="header__heading--primary">Safety & </span> 
+            <span class="header__heading--secondary">Self-defense</span> 
+
+        </h1>
     </div>
 </div>
 
