1bd13236c8e1207a3dbbb603343c1b528114251a
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-05-21 17:50:33 +0800

    redesign login.php - solve issue with css not loading - browser cache

--- header.php
+++ header.php
@@ -8,8 +8,8 @@
     <title>HOME PAGE</title>
     <link rel="stylesheet"  href="css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet"> 
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet"> 
   
 </head>
 <body>
--- login.php
+++ login.php
@@ -1,14 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet"  href="styles.css">
+</head>
+<body>
+
 <?php 
     include_once 'header.php';
 ?>
 
 <section>
-    <div class="signupForm">
+    <div class="loginForm">
         <h2>LOG IN</h2>
-        <form class="signupForm__form" action="login.inc.php" method="POST">
-            <input type="text" name="name" placeholder="Username/Email">
-            <input type="password" name="pwd" placeholder="Password">
-            <button type="submit" name="submit">Login</button>
+        <form class="loginForm__form" action="login.inc.php" method="POST">
+            <input class="loginForm__input" type="text" name="name" placeholder="Username/Email">
+            <input class="loginForm__input" type="password" name="pwd" placeholder="Password">
+            <button class="loginForm__button" type="submit" name="submit">Login</button>
 
         </form>
     </div>
@@ -18,3 +29,6 @@
 <?php 
 include_once 'footer.php';
 ?>
+
+</body>
+</html>
