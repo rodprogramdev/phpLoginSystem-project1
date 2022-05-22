d177f0d8574ea806708ada2483cf9d32fdff9e6c
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-05-22 20:52:15 +0800

    create functions.inc.php in includes folder

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,7 +1,17 @@
 <?php
 if(isset($_POST["submit"])){
     //If this is done inside of the code
-    echo "It works";
+    // echo "It works";
+    $name = $_POST["name"]; // referencing hte signup.php input element with name attribute
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwdrepeat"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+
 }
 else{
     header("location: ../signup.php");
