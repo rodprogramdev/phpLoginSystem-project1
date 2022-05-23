70643c95b7fca56261ff35ce50ba7266804f729f
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-05-23 09:19:57 +0800

    made error message display on screen and solve error message undefined offset on line 32

--- signup.php
+++ signup.php
@@ -26,7 +26,35 @@
 
         </form>
     </div>
+    
+<?php
+    if(isset($_GET["error"])){ // check if a certain url is present 
+        if($_GET["error"] == "emptyinput"){
+            echo "<p>Fill in all fields!</p>";
+        }
+        else if($_GET["error"] == "invaliduid"){
+            echo "<p>Choose a proper username</p>";
+        }
+        else if($_GET["error"] == "invalidemail"){
+            echo "<p>Choose a proper email!</p>";
+        }
+        else if($_GET["error"] == "passwordsdontmatch"){
+            echo "<p>Passwords doensn't match username</p>";
+        }
+        else if($_GET["error"] == "stmtfailed"){
+            echo "<p>Something went wrong, try again!</p>";
+        }
+        else if($_GET["error"] == "usernametaken"){
+            echo "<p>Username already taken!</p>";
+        }
+        else if($_GET["error"] == "none"){
+            echo "<p>You have signed up!</p>";
+        }
+    }
+?>
 </section>
+
+
 <?php 
 include_once 'footer.php';
 ?>
