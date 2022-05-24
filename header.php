6a6ba5bd859ebc9ec9e58e7e52563d66bc1b02dc
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-05-24 15:25:21 +0800

    bootstrap carousel working

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME PAGE</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet"> 
--- index.php
+++ index.php
@@ -3,38 +3,35 @@ include_once 'header.php';
 ?>
 
 
-<div class="header" id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-<div class="carousel-item active">
-    <div  class="carousel-inner">
-       <img src="images/banner.png" class="d-block w-100" alt="..."> 
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="header__heading">
-   
-        <h1>
-       
-            <span class="header__heading--primary">Safety & </span> 
-            <span class="header__heading--secondary">Self-defense</span> 
+<div class="header" id="carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div  class="carousel-item active">
+        <img src="images/selfdefense.jpg" class="d-block w-100" alt="..."> 
+        </div>
+        <div class="carousel-item">
+        <img src="images/firstaid.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="images/self-defense2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="images/firstaid.jpg" class="d-block w-100" alt="...">
+        </div>
 
-        </h1>
-        <h2>
-        <?php
-             if(isset($_SESSION["useruid"])){ // show something depending if the user is login or not
-                echo "<p>Welcome! " . $_SESSION["useruid"] . "</p>";
-             }
-          
-            ?>
-    
-        </h2>
-   
-     
-    
-    </div>
+        <div class="header__heading">
+            <h1>
+                <span class="header__heading--primary">Safety & </span> 
+                <span class="header__heading--secondary">Self-defense</span> 
+            </h1>
+            <h2>
+            <?php
+                if(isset($_SESSION["useruid"])){ // show something depending if the user is login or not
+                    echo "<p>Welcome! " . $_SESSION["useruid"] . "</p>";
+                }
+            
+                ?>
+            </h2>
+        </div>
     
     </div>
 </div>
@@ -47,5 +44,8 @@ include_once 'header.php';
 <?php 
 include_once 'footer.php';
 ?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
