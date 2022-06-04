b91e938095248fdd5eeba77ac160f46eda87e909
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-06-04 22:56:38 +0800

    add lesson 1 and lesson 2

--- courses.php
+++ courses.php
@@ -14,7 +14,24 @@ include_once 'header.php';
         src="https://www.youtube.com/embed/UZKZgXsOEVk">
 </iframe> 
     </div>
+    <section>
+        <div class="courses__lesson1">
+            <h2 class="courses__lesson1--h2">Lesson 1: Proper Holding of Sticks</h1>
+              <iframe width="800" height="500"
+        src="https://www.youtube.com/embed/cbde53P8Hrk">
+            </iframe> 
+        </div>
     
+    </section>
+    <section>
+        <div class="courses__lesson2">
+            <h2 class="courses__lesson1--h2">Lesson 2: Different Weapon Hand Positioning / Chambering</h1>
+              <iframe width="800" height="500"
+        src="https://www.youtube.com/embed/04sq_IDZxwA">
+            </iframe> 
+        </div>
+    
+    </section>
 
 
 
